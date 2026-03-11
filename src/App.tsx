175418a5646e04128023f5e0343091fbc31e6e79
IqraL <IqraLatif159@gmail.com>
2026-03-11 21:29:06 +0000

    added a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function ErrorFallback({ error }: { error: unknown }) {
+  useEffect(() => {
+    //TODO:
+    //Add to logging service
+    //error.message
+    console.log("error boundary hit");
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      Something went wrong
+      <pre>{error.message}</pre>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          height: "100vh",
+          display: "grid",
+          gridTemplateRows: "2fr 10fr",
+        }}
+      >
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Header />
+        </ErrorBoundary>
+        <div>H</div>
+      </div>
+    </>
+  );
+}
+
+const Header = () => {
+  return <div>H</div>;
+};
+export default App;
